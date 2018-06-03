848946e438d5c56fdcee687eb4d458e0b0ff1a09
Lorenzo Relos <lrelos@ucsc.edu>
2018-06-03 02:29:24 -0700

    Added new sprites for wall clinging, dashing up, and dashing down, optimized implementation using scale, renamed assets to 'ninja', resolved character not idling when hitting the ground without further input. Issue with sprite extending into wall persists.

--- myGame/js/Player.js
+++ myGame/js/Player.js
@@ -15,22 +15,18 @@ var jumpTime; // var timer so  player can have a bit more time to wall jump
 
 function Player(game, x, y, frame) {
 	// call to Phaser.sprite // new sprite (game, x, y, key, frame)
-	Phaser.Sprite.call(this, game, x, y, 'player', 'rightIdle');
+	Phaser.Sprite.call(this, game, x, y, 'ninja', 'idle');
+	this.anchor.setTo(0.5, 0.5);
+    this.scale.setTo(0.3);
 
-    this.animations.add('rightRun', Phaser.Animation.generateFrameNames('rightRun', 1, 8), 10, true);
-    this.animations.add('leftRun', Phaser.Animation.generateFrameNames('leftRun', 1, 8), 10, true);
+    this.animations.add('run', Phaser.Animation.generateFrameNames('run', 1, 8), 10, true);
 
 	// add properties
     game.physics.arcade.enable(this);
 
-	this.anchor.setTo(0.5, 0.5);
-    this.scale.setTo(0.3);
     this.body.collideWorldBounds = true;
     this.body.gravity.y = gameOptions.playerGravity;
-
-    this.playerIdleLeft = false;
-    this.playerIdleRight = false;
-
+    this.frameName = 'jump';
 }
 
 Player.prototype = Object.create(Phaser.Sprite.prototype);
@@ -42,14 +38,20 @@ Player.prototype.constructor = Player;
 
 Player.prototype.update = function() {
 
+		if(this.body.velocity.x > 0){
+			this.scale.x = 0.3;
+		}else if (this.body.velocity.x < 0){
+			this.scale.x = -0.3;
+		}
+
     //stops player velocity after jumping
         if (this.body.touching.down) {
             inAir = false;
             wallJumpLeft = false;
             wallJumpRight = false;
             this.body.velocity.x = 0;
-            if (this.scale.x < 0) {
-                this.scale.x *= -1;
+            if (!game.input.keyboard.isDown(Phaser.Keyboard.LEFT) && !game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            	this.frameName = 'idle';
             }
         }else{
             inAir = true;
@@ -57,55 +59,31 @@ Player.prototype.update = function() {
 
         //running animsation for left and right movement
         if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT) && !inAir){
-            //this.scale.x = -1;
+        	this.scale.x = -0.3
             this.body.velocity.x = -gameOptions.playerSpeed;
-            this.animations.play('leftRun');
+            this.animations.play('run');
         } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) && !inAir){
-            //this.scale.x *= 1;
+        	this.scale.x = 0.3
             this.body.velocity.x = gameOptions.playerSpeed;
-            this.animations.play('rightRun');
+            this.animations.play('run');
         } 
 
 
 
         //stops velocity and animation when releasing button  for the right
-        if (game.input.keyboard.justReleased(Phaser.Keyboard.RIGHT)){
+        if (game.input.keyboard.justReleased(Phaser.Keyboard.RIGHT) || game.input.keyboard.justReleased(Phaser.Keyboard.LEFT)){
             if(inAir == false) {
                 this.animations.stop();
-                this.frameName = 'rightIdle';
+                this.frameName = 'idle';
                 this.body.velocity.x = 0;
             } // stops it for the left
-        } else if (game.input.keyboard.justReleased(Phaser.Keyboard.LEFT)){
-            if(inAir == false) {
-                this.animations.stop();
-                this.frameName = 'leftIdle';
-                this.body.velocity.x = 0;
-            }
-        } 
-
-        if(inAir){
-            if(this.body.velocity.x <0){
-                //if (this.scale.x < 0) {
-                //    this.scale.x *= -1;
-                //}
-                this.animations.stop();
-                this.frameName = 'leftJump';
-            }else if (this.body.velocity.x >0){
-                //if (this.scale.x < 0) {
-                //    this.scale.x *= -1;
-                //}       
-                this.animations.stop();
-                this.frameName = 'rightJump';
-            }
         }
-
-
-
-
 
         // Can only jump once while  on top of a platform
         if (this.body.touching.down && !inAir) {
             if(!jumpButtonDown && game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+            	this.animations.stop();
+            	this.frameName = 'jump';
                 this.body.velocity.y = -gameOptions.playerJump;
                 jumpButtonDown = true;
             }
@@ -122,18 +100,19 @@ Player.prototype.update = function() {
         // and if player is touching wall
         if(inAir){
         if (this.body.touching.right) {
-            jumpTime = game.time.time + 500;
+        	this.animations.stop();
             wallJumpRight = true;
-
+            this.frameName = 'wallCling'
         } else if(this.body.touching.left) {
-            jumpTime = game.time.time + 500;
+        	this.animations.stop();
             wallJumpLeft = true;
+            this.frameName = 'wallCling'
         }
-    }
+}
 
         //Wall jump Right
         if (wallJumpRight && game.input.keyboard.justReleased(Phaser.Keyboard.SPACEBAR) && game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            this.frameName = 'leftJump';
+            this.frameName = 'jump';
             this.body.velocity.y = -gameOptions.playerWallJump;
             this.body.velocity.x = -gameOptions.playerForce;
             wallJumpRight = false;
@@ -143,7 +122,7 @@ Player.prototype.update = function() {
 
         //Wall jump Left
          if (wallJumpLeft && game.input.keyboard.justReleased(Phaser.Keyboard.SPACEBAR) && game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-            this.frameName = 'rightJump';
+            this.frameName = 'jump';
             this.body.velocity.y = -gameOptions.playerWallJump;
             this.body.velocity.x =  gameOptions.playerForce;
             wallJumpLeft = false;
--- myGame/js/loading.js
+++ myGame/js/loading.js
@@ -13,7 +13,7 @@
 		game.load.spritesheet("level_arrows", "level_arrows.png", 48, 48);
 		game.load.atlas('atlas', 'PHSpritesheet.png', 'PHsprites.json');
 		game.load.atlas('Mnt', 'tempMnt.png', 'tempMnt.json');
-	    game.load.atlas('player', 'player.png', 'player.json');
+	    game.load.atlas('ninja', 'ninja.png', 'ninja.json');
 		game.load.atlas('ground', 'ground.png', 'ground.json');
 		game.load.audio('bgMusic', ['../audio/Ninja_Background.mp3'], ['../audio.Ninja_Background.ogg']);
 	},
