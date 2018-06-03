7c724805945f16b181455efe9fa8556bfc612166
Lorenzo Relos <lrelos@ucsc.edu>
2018-06-03 12:41:49 -0700

    Implemented the dash and new sprites, but it's still a limitless dash.

--- myGame/js/Player.js
+++ myGame/js/Player.js
@@ -11,7 +11,7 @@ var jumpButtonDown = false;
 var wallJumpRight = false;
 var wallJumpLeft = false;
 var inAir = false;
-var jumpTime; // var timer so  player can have a bit more time to wall jump
+var dash = 0;
 
 function Player(game, x, y, frame) {
 	// call to Phaser.sprite // new sprite (game, x, y, key, frame)
@@ -58,14 +58,18 @@ Player.prototype.update = function() {
         }
 
         //running animsation for left and right movement
-        if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT) && !inAir){
+        if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
         	this.scale.x = -0.3
-            this.body.velocity.x = -gameOptions.playerSpeed;
-            this.animations.play('run');
-        } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) && !inAir){
+        	if(!inAir){
+            	this.body.velocity.x = -gameOptions.playerSpeed;
+            	this.animations.play('run');
+            }
+        } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
         	this.scale.x = 0.3
-            this.body.velocity.x = gameOptions.playerSpeed;
-            this.animations.play('run');
+        	if(!inAir){
+            	this.body.velocity.x = gameOptions.playerSpeed;
+            	this.animations.play('run');
+            }
         } 
 
 
@@ -99,16 +103,22 @@ Player.prototype.update = function() {
         // Checks if player is in Air, holding right against all to the right,
         // and if player is touching wall
         if(inAir){
-        if (this.body.touching.right) {
-        	this.animations.stop();
-            wallJumpRight = true;
-            this.frameName = 'wallCling'
-        } else if(this.body.touching.left) {
-        	this.animations.stop();
-            wallJumpLeft = true;
-            this.frameName = 'wallCling'
-        }
-}
+        	if (this.body.touching.right) {
+        		this.animations.stop();
+            	wallJumpRight = true;
+            	this.frameName = 'wallCling'
+        	} else if(this.body.touching.left) {
+        		this.animations.stop();
+            	wallJumpLeft = true;
+            	this.frameName = 'wallCling'
+        	}
+		}
+
+		if((wallJumpRight && game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) || wallJumpLeft && game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+			this.frameName = 'jump';
+			wallJumpRight = false;
+			wallJumpLeft = false;
+		}
 
         //Wall jump Right
         if (wallJumpRight && game.input.keyboard.justReleased(Phaser.Keyboard.SPACEBAR) && game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
@@ -118,8 +128,6 @@ Player.prototype.update = function() {
             wallJumpRight = false;
         }
 
-    
-
         //Wall jump Left
          if (wallJumpLeft && game.input.keyboard.justReleased(Phaser.Keyboard.SPACEBAR) && game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
             this.frameName = 'jump';
@@ -128,5 +136,65 @@ Player.prototype.update = function() {
             wallJumpLeft = false;
         }
 
+ // *********************
+        // This is the WIP dash code
+        // *********************
 
+        // Checks to see if either the right arrow or left arrow have been pressed 
+        // so that the dash can done
+        if(game.input.keyboard.isDown(Phaser.Keyboard.D) && this.scale.x < 0){
+            this.body.velocity.x = -600;
+            this.body.gravity.y = 0;
+            this.body.velocity.y = 0;
+            this.animations.stop();
+            this.frameName = 'run2';
+            //dashBar -= 1;
+        } else if (game.input.keyboard.isDown(Phaser.Keyboard.D) && this.scale.x > 0){
+        	this.body.gravity.y = 0;
+        	this.body.velocity.y = 0;
+            this.body.velocity.x = 600;
+            this.animations.stop();
+            this.frameName = 'run2';
+            //dashBar -= 1;
+        } 
+
+        // When you click up arrow only, sets values to false so that the player can dash only up
+
+        if (game.input.keyboard.isDown(Phaser.Keyboard.UP) && game.input.keyboard.isDown(Phaser.Keyboard.D)){
+        	this.body.gravity.y = 0;
+            this.body.velocity.y = -600; 
+            this.body.velocity.x = 0;
+            this.frameName = 'dashUp';
+        }
+
+        if (inAir && game.input.keyboard.isDown(Phaser.Keyboard.DOWN) && game.input.keyboard.isDown(Phaser.Keyboard.D)){
+        	this.body.gravity.y = 0;
+            this.body.velocity.y = 600;
+            this.body.velocity.x = 0; 
+            this.frameName = 'dashDown';
+        }
+        // this is the wip dash code for diagonal down to the right
+        // if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN) && game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) && rightFacing === true){
+        //     this.body.velocity.x = 600;
+        //     this.body.velocity.y = 600;
+        // }
+
+        // Stops the running animation after the dash button is released 
+        if(game.input.keyboard.justReleased(Phaser.Keyboard.D)){
+        	this.body.gravity.y = gameOptions.playerGravity;
+        	if(inAir){
+        		if(wallJumpRight || wallJumpLeft){
+        			this.frameName = 'wallCling';
+        		}else{
+        			this.frameName = 'jump';
+        		}
+        	}else{
+        		this.frameName = 'idle';
+        	}
+        }
+        // if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR) && game.input.keyboard.isDown(Phaser.Keyboard.D) ){
+        //     dash = false;
+        //     walljump = false;
+        // }
+        // *********************
 }
