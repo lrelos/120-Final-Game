7e981e0f7713405b9ffc0ebeb7a46a7046fd28c3
Jherna123 <38268387+Jherna123@users.noreply.github.com>
2018-06-09 21:45:55 -0700

    Level 3

---
+++ myGame/js/Level3.js
@@ -0,0 +1,148 @@
+var dashBar = null;
+
+Level3 = {
+	create: function() {
+
+		this.total = 0;
+		this.timer;
+   		this.timeText;
+		this.bronzeTime = 120;
+		this.silverTime = 90;
+		this.goldTime = 60;
+		this.stars = 0;
+
+		// Plays Background Music
+        this.music = this.add.audio('bgMusic4');
+        this.music.play();
+        this.music.loop;
+
+        //creates timer object
+        timer = this.time.create(false);
+        timer.loop(1000, this.updateTime, this);
+        timer.start();
+
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		game.physics.arcade.TILE_BIAS = 32;
+
+		// adds the background image. Separate from tile
+	    this.background = game.add.tileSprite(0, 0, 3850, 3500, 'lvl3Background');
+
+	    // creates the tile map
+		lvl3Map = game.add.tilemap('Level3');
+		lvl3Map.addTilesetImage('Tundra', 'iceTiles');
+
+		// sets collision by exclusion. Player will collide with everything except these numbers
+		lvl3Map.setCollisionByExclusion([0], true);
+
+		// creates the layer and reiszes the world to match
+		lvl3Layer = lvl3Map.createLayer('iceTerrain');
+		lvl3Layer.resizeWorld();
+
+
+		// adding scrolls to fill up dash meter
+		// creates scroll objects from tiled
+		this.dashScrolls = game.add.group();
+		this.dashScrolls.enableBody = true;
+		// parameters(Object layer name, named of objects tile reference or gid #, image key name, frame #, exists, autocall, named of group to add objects to)
+		lvl3Map.createFromObjects('Items', 'scroll', 'scrolls', 0, true, false, this.dashScrolls);
+
+		this.rideBlocks = game.add.group();
+		this.rideBlocks.enableBody = true;
+		lvl3Map.createFromObjects('Items', 'rideBlock', 'rideblocks', 0, true, false, this.rideBlocks);
+
+		this.rideBlocks.forEach(function(rideBlock){
+        rideBlock.body.immovable = true;
+        rideBlock.body.velocity.y = -100;
+    	});
+
+
+		// creates new player for this level
+		this.player = new Player(game, 600, 3260); 
+        game.add.existing(this.player);
+        //Phaser.Camera.FOLLOW_PLATFORMER = 1;
+        game.camera.follow(this.player);;
+
+        // adds the dash bar
+		this.dashBar = this.add.sprite(20, 60, 'dashBar');
+		this.dashBar.scale.y = 0.5;
+		this.dashBar.fixedToCamera = true;
+
+		//changes friction for ice level for slippery terrain
+		frictionDragX = 250;
+
+	},
+
+	update: function() {
+
+		// adds collision for player and tiled map
+		game.physics.arcade.collide(this.player, lvl3Layer);
+		game.physics.arcade.collide(this.player, this.rideBlocks);
+		// adds overlap for player and scroll
+		game.physics.arcade.overlap(this.player, this.dashScrolls, collectScroll, null, this);
+
+
+		// kills and resets player if they fall off the world
+		if (this.player.body.y > (game.world.height + this.player.body.height/2)) {
+			this.player.kill();
+
+			// creates new player if player dies or falls of world
+			this.player = new Player(game, 600, 3260); 
+        	game.add.existing(this.player);
+        	//Phaser.Camera.FOLLOW_PLATFORMER = 1;
+        	game.camera.follow(this.player);
+        	lvl3Map.createFromObjects('Items', 'scroll', 'scrolls', 0, true, false, this.dashScrolls);
+ 		}
+
+ 		// function when player collects scroll
+		function collectScroll(player, scroll) {
+			scroll.kill(); // kills scroll
+			dash += 60; // adds to the dash meter
+		}
+
+
+		// changes friction when riding on a non-icy platform
+		if (this.player.body.touching.down) {
+			frictionDragX = 2500;
+		}
+		else if (!this.player.body.touching.down) {
+			frictionDragX = 250;
+		}
+
+		// adds traits for the riding platform blocks
+	    this.rideBlocks.forEach(function(rideBlock){
+      	if (rideBlock.body.y < 900) {
+      		rideBlock.body.velocity.y = 250; // raises it up to certain height
+      	}
+      	if (rideBlock.body.y > 2500) {
+      		rideBlock.body.velocity.y = -250; // lowers it to certain height
+      	}
+    	});
+
+
+		// amount of stars we recieve
+		if(this.total < this.goldTime) {
+			this.stars = 3;
+		}
+		else if(this.total < this.silverTime) {
+			this.stars = 2;
+		}
+		else if(this.total < this.bronzeTime) {
+			this.stars = 1;
+		}
+		else this.stars = 0;
+	},
+
+	render: function() {
+		// show timer01 debug text
+		game.debug.text('Time Elapsed: ' + this.total, 32, 32, "#ff3333", '40px');
+		game.debug.text('Stars: ' + this.stars, 50, 50, "#FFFFFF", '72px');
+		this.dashBar.scale.x = this.player.getDashScale();
+	},
+
+	// updates the time
+	updateTime: function(){
+		this.total++;
+	}
+
+
+}
