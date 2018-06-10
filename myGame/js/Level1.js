e0f6740427a3e52dc3d56457923d696dc9a50c1c
Jherna123 <38268387+Jherna123@users.noreply.github.com>
2018-06-10 00:38:19 -0700

    Updated js

--- myGame/js/Level1.js
+++ myGame/js/Level1.js
@@ -12,7 +12,7 @@ Level1 = {
 		this.stars = 0;
 
 		// Plays Background Music
-        this.music = this.add.audio('bgMusic');
+        this.music = this.add.audio('bgMusic1');
         this.music.play();
         this.music.loop;
 
@@ -40,10 +40,10 @@ Level1 = {
 
 		// adding scrolls to fill up dash meter
 		// creates scroll objects from tiled
-		//this.dashScrolls = game.add.group();
-		//this.dashScrolls.enableBody = true;
+		this.dashScrolls = game.add.group();
+		this.dashScrolls.enableBody = true;
 		// parameters(Object layer name, named of objects tile reference or gid #, image key name, frame #, exists, autocall, named of group to add objects to)
-		//lvl2Map.createFromObjects('Items', 'scroll', 'scrolls', 0, true, false, this.dashScrolls);
+		lvl1Map.createFromObjects('Items', 'scroll', 'scrolls', 0, true, false, this.dashScrolls);
 
 
 		// creates new player for this level
@@ -57,6 +57,16 @@ Level1 = {
 		this.dashBar.scale.y = 0.5;
 		this.dashBar.fixedToCamera = true;
 
+		this.flags = game.add.group();
+		this.flags.enableBody = true;
+		lvl1Map.createFromObjects('Items', 'flag', 'flags', 2, true, false, this.flags);
+		// sets attributes for  flags
+		this.flags.forEach(function(flag){
+        	flag.body.immovable = true;
+        	flag.animations.add('flagWave', [0, 1], 2, true);
+			flag.animations.play('flagWave');
+		});
+
 	},
 
 	update: function() {
@@ -64,12 +74,43 @@ Level1 = {
 		// adds collision for player and tiled map
 		game.physics.arcade.collide(this.player, lvl1Layer);
 		// adds overlap for player and scroll
-		//game.physics.arcade.overlap(this.player, this.dashScrolls, collectScroll, null, this);
+		game.physics.arcade.overlap(this.player, this.dashScrolls, collectScroll, null, this);
+		// adds overlap for player and flag
+		game.physics.arcade.overlap(this.player, this.flags, reachFlag, null, this);
 
-		/*function collectScroll(player, scroll) {
+		if (this.player.body.y > (game.world.height + this.player.body.height/4)) {
+			this.player.kill();
+
+			// creates new player if player dies or falls of world
+			this.player = new Player(game, 150, 2425); 
+        	game.add.existing(this.player);
+        	//Phaser.Camera.FOLLOW_PLATFORMER = 1;
+        	game.camera.follow(this.player);
+ 		}
+
+		function collectScroll(player, scroll) {
 			scroll.kill(); // kills scroll
-			dash = 90; // adds to the dash meter
-		}*/
+			dash += 60; // adds to the dash meter
+		}
+
+		// function for when player reaches flag
+		function reachFlag(player, flag) {
+			//stops the timer
+			timer.stop();
+	
+			this.music.stop();
+			frictionDragX = 2500;
+				
+			// did we improved our stars in current level?
+			if(game.global.starsArray[game.global.level-1] < this.stars){
+				game.global.starsArray[game.global.level-1] = this.stars;
+			}
+			// if we completed a level and next level is locked - and exists - then unlock it
+			if( this.stars > 0 && game.global.starsArray[game.global.level]==4 && game.global.level<game.global.starsArray.length){
+				game.global.starsArray[game.global.level] = 0;
+			}
+			game.state.start('LevelSelect');
+		}
 
 		// amount of stars we recieve
 		if(this.total < this.goldTime) {
