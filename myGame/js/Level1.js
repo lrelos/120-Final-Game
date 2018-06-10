80ec6fe86c9baaed2e5b8fc33d7940f0ecd3af09
Lorenzo Relos <lrelos@ucsc.edu>
2018-06-10 14:19:58 -0700

    Fixed dash bar scale implementaion in some of the levels still using the old conversion.

--- myGame/js/Level1.js
+++ myGame/js/Level1.js
@@ -134,7 +134,7 @@ Level1 = {
 		// show timer01 debug text
 		game.debug.text('Time Elapsed: ' + this.total, 32, 32, "#ff3333", '40px');
 		game.debug.text('Stars: ' + this.stars, 50, 50, "#FFFFFF", '72px');
-		this.dashBar.scale.x = dash*(1/90);
+		this.dashBar.scale.x = this.player.getDashScale();
 	},
 
 	// updates the time
